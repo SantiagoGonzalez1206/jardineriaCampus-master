4bb976ed680b19c9a41be019541d4056aa4c75be
SantiagoGonzalez1206 <160767943+SantiagoGonzalez1206@users.noreply.github.com>
2024-03-13 06:16:39 -0500

    a

--- modules/getClientes.py
+++ modules/getClientes.py
@@ -85,20 +85,23 @@ def getClienteNoPagos():
 def menu():
   while True:  
     print("""
-    ____                        __                   __        __                   ___            __           
-   / __ \___  ____  ____  _____/ /____  _____   ____/ /__     / /___  _____   _____/ (_)__  ____  / /____  _____
-  / /_/ / _ \/ __ \/ __ \/ ___/ __/ _ \/ ___/  / __  / _ \   / / __ \/ ___/  / ___/ / / _ \/ __ \/ __/ _ \/ ___/
- / _, _/  __/ /_/ / /_/ / /  / /_/  __(__  )  / /_/ /  __/  / / /_/ (__  )  / /__/ / /  __/ / / / /_/  __(__  ) 
-/_/ |_|\___/ .___/\____/_/   \__/\___/____/   \__,_/\___/  /_/\____/____/   \___/_/_/\___/_/ /_/\__/\___/____/  
-          /_/                                                                                                   
+
+______                      _             _        _             _____ _ _            _            
+| ___ \                    | |           | |      | |           /  __ \ (_)          | |           
+| |_/ /___ _ __   ___  _ __| |_ ___    __| | ___  | | ___  ___  | /  \/ |_  ___ _ __ | |_ ___  ___ 
+|    // _ \ '_ \ / _ \| '__| __/ _ \  / _` |/ _ \ | |/ _ \/ __| | |   | | |/ _ \ '_ \| __/ _ \/ __|
+| |\ \  __/ |_) | (_) | |  | ||  __/ | (_| |  __/ | | (_) \__ \ | \__/\ | |  __/ | | | ||  __/\__ \
+\_| \_\___| .__/ \___/|_|   \__\___|  \__,_|\___| |_|\___/|___/  \____/_|_|\___|_| |_|\__\___||___/
+          | |                                                                                      
+          |_|                                                                                      
 
 
-                                 1. Obtener todos los clientes de España
-                                 2. Obtener los clientes de Madrid cuyo codigo de representante de ventas sea 11 o 30
-                                 3. Obtener nombre de cada cliente y nombre y apellido de su representante de ventas
-                                 4. Obtener el nombre de los clientes que hayan realizado pagos y el nombre de sus representantes de ventas
-                                 5. Obtener el nombre de los clientes que NO hayan realizado pagos y el nombre de sus representantes de ventas
-                                 6. Salir
+             1. Obtener todos los clientes de España
+             2. Obtener los clientes de Madrid cuyo codigo de representante de ventas sea 11 o 30
+             3. Obtener nombre de cada cliente y nombre y apellido de su representante de ventas
+             4. Obtener el nombre de los clientes que hayan realizado pagos y el nombre de sus representantes de ventas
+             5. Obtener el nombre de los clientes que NO hayan realizado pagos y el nombre de sus representantes de ventas
+             6. Salir
 """)
     
     opcion= int(input("\nSeleccione una de las opciones: "))
